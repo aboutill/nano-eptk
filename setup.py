40e5a6fb9d39be2b0909cee8a785af1d1f943810
Arnaud Boutillon <arnaud.boutillon@kcl.ac.uk>
2026-07-07 11:28:49 +0100

    setup.py update

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ setup(
     name="nano-eptk",
     entry_points={
         "console_scripts": [
-            "mspoc = bin.mspoc:main",
-            "mspocr = bin.mspocr:main",
-            "poc = bin.poc:main",
-            "pocr = bin.pocr:main",
-            "saep = bin.saep:main",
-            "dhcp_gre_saep = bin.dhcp_gre_saep:main",
-            "dhp_epi_poc = bin.dhp_epi_poc:main",
-            "dhp_epi_pocr = bin.dhp_epi_pocr:main",
-            "dhcp_tse_mspoc = bin.dhcp_tse_mspoc:main",
-            "dhcp_tse_mspocr = bin.dhcp_tse_mspocr:main",
+            "mspoc = nano_eptk.cli.mspoc:main",
+            "mspocr = nano_eptk.cli.mspocr:main",
+            "poc = nano_eptk.cli.poc:main",
+            "pocr = nano_eptk.cli.pocr:main",
+            "saep = nano_eptk.cli.saep:main",
+            "dhcp_gre_saep = nano_eptk.cli.dhcp_gre_saep:main",
+            "dhcp_epi_poc = nano_eptk.cli.dhcp_epi_poc:main",
+            "dhcp_epi_pocr = nano_eptk.cli.dhcp_epi_pocr:main",
+            "dhcp_tse_mspoc = nano_eptk.cli.dhcp_tse_mspoc:main",
+            "dhcp_tse_mspocr = nano_eptk.cli.dhcp_tse_mspocr:main",
+            "nano-eptk = nano_eptk.cli.dispatch:main",
             ]
         }
 )
